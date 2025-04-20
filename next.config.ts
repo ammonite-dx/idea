5aba37c30e5df846bb05c4b6726fd93c09b00484
Ammonitedx <ammonitedx@gmail.com>
2025-04-21 01:45:27 +0900

    Try build on Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // App Router + SSR対応
 };
 
 export default nextConfig;
