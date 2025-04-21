573365798519876fd423ee9634a023c414aabf19
Ammonitedx <ammonitedx@gmail.com>
2025-04-22 00:41:46 +0900

    [auth][error] UnknownAction: Unsupported action.修正2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,6 @@
 export const runtime = "edge";
 
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { handlers } from "@/auth";
 
-const handler = NextAuth(authOptions);
-
-export const GET  = handler;
-export const POST = handler;
+export const GET  = handlers.GET;
+export const POST = handlers.POST;
