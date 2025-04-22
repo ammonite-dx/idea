13d86df491485e6ea55f5fa0b15810e9163e53ac
Ammonitedx <ammonitedx@gmail.com>
2025-04-22 13:09:18 +0900

    route.tsにエラー時にログを出力する機能を追加

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,22 @@
-export const runtime = "edge";
+export const runtime = "edge"
 
 import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+import type { NextRequest } from "next/server"
+
+export const GET = async (request: NextRequest) => {
+  try {
+    return await handlers.GET(request)
+  } catch (error) {
+    console.error("[NEXTAUTH HANDLER GET ERROR]", error)
+    throw error
+  }
+}
+
+export const POST = async (request: NextRequest) => {
+  try {
+    return await handlers.POST(request)
+  } catch (error) {
+    console.error("[NEXTAUTH HANDLER POST ERROR]", error)
+    throw error
+  }
+}
