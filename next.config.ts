9526189ef0e8a3a7955f14c134cf806ed6dd1628
SoraHirokane <sorascience@gmail.com>
2025-04-29 20:46:00 +0900

    本番ビルドでwebpackミニファイをスキップ

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  webpack: (config, { dev }) => {
 
+    // 本番ビルドで Webpack ミニファイをスキップ
+    if (!dev) {
+      config.optimization = config.optimization || {}
+      config.optimization.minimize = false
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
