f34fd91ae8fda7c28c01918258c6dc925653d327
SoraHirokane <sorascience@gmail.com>
2025-04-29 20:32:29 +0900

    ビルド時エラー修正

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
       util: false,
       url: false,
       assert: false,
+      zlib: false,
+      child_process: false,
+      readline: false,
+      module: false,
+      worker_threads: false,
+      events: false,
     }
 
     config.optimization = config.optimization || {}
