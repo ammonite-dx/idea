7c3d46eced171822fe1d5b86961a808ed5b7e9ce
SoraHirokane <sorascience@gmail.com>
2025-04-29 14:33:26 +0900

    ビルド時のエラー修正

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-export function SignIn() {
+export default function page() {
   const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
