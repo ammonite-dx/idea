d378fa41e91d6a13211289bb25fcd32cd9fe1693
SoraHirokane <sorascience@gmail.com>
2025-04-29 20:03:06 +0900

    ビルド時エラー修正

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from 'next'
 import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+import webpack from 'webpack'
 
 initOpenNextCloudflareForDev()
 
 const nextConfig: NextConfig = {
-  webpack(config) {
+  webpack(config, { dev }) {
     // Edge Runtime 向けに Node.js ビルトインを除外
     config.resolve.fallback = {
       ...(config.resolve.fallback || {}),
@@ -20,6 +21,13 @@ const nextConfig: NextConfig = {
       util: false,
       url: false,
       assert: false,
+    }
+
+    // 開発時にのみ miniflare を無視
+    if (dev) {
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^miniflare$/ })
+      )
     }
 
     return config
