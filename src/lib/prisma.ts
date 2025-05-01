736cba4b61c0014b550cc9f8ca3a2087037bf034
SoraHirokane <sorascience@gmail.com>
2025-05-01 09:41:29 +0900

    ESLintエラー修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient }   from "@prisma/client";
 import { PrismaD1 }        from "@prisma/adapter-d1";
 import { D1Database }      from "@cloudflare/workers-types";
 
-let cache: { [binding: string]: PrismaClient } = {};
+const cache: { [binding: string]: PrismaClient } = {};
 
 export default function getPrismaClient(env: { DB: D1Database }): PrismaClient {
   // ページ関数ごとに渡ってくる env.DB をキーにキャッシュ
