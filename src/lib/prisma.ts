580579c175c270147c0e2e4dfdadfbe4b91f4ac3
SoraHirokane <sorascience@gmail.com>
2025-05-03 20:42:02 +0900

    ビルド時エラー修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,6 @@ import { PrismaClient }   from "@prisma/client";
 import { PrismaD1 }        from "@prisma/adapter-d1";
 import { D1Database }      from "@cloudflare/workers-types";
 
-const cache: { [binding: string]: PrismaClient } = {};
-
 export default function getPrismaClient(db: D1Database): PrismaClient {
   console.log("getPrismaClient called");
   const prisma = new PrismaClient({
