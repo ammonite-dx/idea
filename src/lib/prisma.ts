a80ed28bc0de3d3755d0f312b8b7e75e07826aa6
SoraHirokane <sorascience@gmail.com>
2025-05-03 21:00:34 +0900

    prisma.tsにログを追加

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,9 @@ import { PrismaD1 }        from "@prisma/adapter-d1";
 import { D1Database }      from "@cloudflare/workers-types";
 
 export default function getPrismaClient(db: D1Database): PrismaClient {
-  console.log("getPrismaClient called");
+  console.log("getPrismaClient received db:", db);
+  console.log("typeof db:", typeof db);
+  console.log("db && Object.keys(db):", db && Object.keys(db));
   db.prepare = db.prepare.bind(db);
   const prisma = new PrismaClient({ adapter: new PrismaD1(db) });
   console.log("PrismaClient created", prisma);
