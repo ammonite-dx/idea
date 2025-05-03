463f7d505f6a4d9045af35ea2fb69d3618f667fb
SoraHirokane <sorascience@gmail.com>
2025-05-03 20:49:50 +0900

    prisma.tsでbindし直し

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,8 @@ import { D1Database }      from "@cloudflare/workers-types";
 
 export default function getPrismaClient(db: D1Database): PrismaClient {
   console.log("getPrismaClient called");
-  const prisma = new PrismaClient({
-    adapter: new PrismaD1(db),
-  })
+  db.prepare = db.prepare.bind(db);
+  const prisma = new PrismaClient({ adapter: new PrismaD1(db) });
   console.log("PrismaClient created", prisma);
   return prisma;
 }
