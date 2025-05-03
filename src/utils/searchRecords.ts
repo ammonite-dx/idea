7b0f278745562aec2ffc50c000afb6e4fa80a302
SoraHirokane <sorascience@gmail.com>
2025-05-03 17:59:43 +0900

    ビルド時エラー修正

--- src/utils/searchRecords.ts
+++ src/utils/searchRecords.ts
@@ -14,11 +14,11 @@ export default async function searchRecords<K extends keyof TypeMap>(
       case "コネ": return (await (await fetch(`/api/connection`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
       case "一般アイテム": return (await (await fetch(`/api/general`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
       default:
-        const weapons = (await (await fetch(`/api/weapon`,{method:"POST", body:JSON.stringify(searchParams)})).json());
-        const armors = (await (await fetch(`/api/armor`,{method:"POST", body:JSON.stringify(searchParams)})).json());
-        const vehicles = (await (await fetch(`/api/vehicle`,{method:"POST", body:JSON.stringify(searchParams)})).json());
-        const connections = (await (await fetch(`/api/connection`,{method:"POST", body:JSON.stringify(searchParams)})).json());
-        const generals = (await (await fetch(`/api/general`,{method:"POST", body:JSON.stringify(searchParams)})).json());
+        const weapons = (await (await fetch(`/api/weapon`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
+        const armors = (await (await fetch(`/api/armor`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
+        const vehicles = (await (await fetch(`/api/vehicle`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
+        const connections = (await (await fetch(`/api/connection`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
+        const generals = (await (await fetch(`/api/general`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
         const items = Object.fromEntries(toArray(searchParams["category"], ITEM_CATEGORIES).map(category => {
           const weaponsInCategory = weapons[category] || [];
           const armorsInCategory: TypeMap[K][] = armors[category] || [];
@@ -28,7 +28,7 @@ export default async function searchRecords<K extends keyof TypeMap>(
           const itemsInCategory: TypeMap[K][] = weaponsInCategory.concat(armorsInCategory).concat(vehiclesInCategory).concat(connectionsInCategory).concat(generalsInCategory);
           return [category, itemsInCategory]
         }));
-        return items as { [key: string]: TypeMap[K][] };
+        return items;
     }
   } else {
     return (await (await fetch(`/api/${kind}`,{method:"POST", body:JSON.stringify(searchParams)})).json()) as { [key: string]: TypeMap[K][] };
