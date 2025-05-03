8f380eab7e3230142a4c4d3c969f419c3c024c3c
SoraHirokane <sorascience@gmail.com>
2025-05-03 20:39:38 +0900

    prisma.tsで毎回新しいクライアントを返す

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,12 +5,10 @@ import { D1Database }      from "@cloudflare/workers-types";
 const cache: { [binding: string]: PrismaClient } = {};
 
 export default function getPrismaClient(db: D1Database): PrismaClient {
-  console.log("getPrismaClient called:", db);
-  const key = "DB";
-  if (!cache[key]) {
-    cache[key] = new PrismaClient({
-      adapter: new PrismaD1(db),
-    });
-  }
-  return cache[key];
+  console.log("getPrismaClient called");
+  const prisma = new PrismaClient({
+    adapter: new PrismaD1(db),
+  })
+  console.log("PrismaClient created", prisma);
+  return prisma;
 }
