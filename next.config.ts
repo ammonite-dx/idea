5b2e7058c56f7b199672085794173caedac038ac
Ammonitedx <ammonitedx@gmail.com>
2025-05-12 21:40:29 +0900

    fix: Error: Cannot find module '@opennextjs/cloudflare'

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from 'next';
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
-import webpack from 'webpack';
-
-initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {};
 
@@ -11,13 +8,6 @@ if (process.env.NODE_ENV === 'development') {
   (async () => {
     try {
       await setupDevPlatform();
-      console.log("Cloudflare development platform (for local D1 access etc.) successfully set up for Next.js.");
-      if (process.env.DB) {
-        console.log("☑️ process.env.DB in next.config.ts IS DEFINED.");
-        console.log("   Type of process.env.DB:", typeof process.env.DB);
-      } else {
-        console.error("‼️ process.env.DB in next.config.ts IS UNDEFINED after setupDevPlatform.");
-      }
     } catch (e) {
       console.error('Failed to set up Cloudflare development platform:', e);
     }
