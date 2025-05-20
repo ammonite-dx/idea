aba43350e0c70b79bc0b5319394b5c748cd9090e
Ammonitedx <ammonitedx@gmail.com>
2025-05-20 11:30:30 +0900

    apiルートにログを追加

--- src/app/api/prisma/route.ts
+++ src/app/api/prisma/route.ts
@@ -7,6 +7,7 @@ export const runtime = 'edge';
 export async function POST(request: Request) {
     try {
         const body = await request.json()
+        console.log("body", body);
         const model = body.model;
         const findOptions = body.findOptions;
 
