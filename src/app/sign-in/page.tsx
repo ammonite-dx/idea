4086cdf76d478970377e4b36a4f0897069fae152
Ammonitedx <ammonitedx@gmail.com>
2025-06-19 23:49:38 +0900

    サインイン済みの時にトップページにリダイレクト

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,27 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
+'use client'; 
+
+import { useUser, useClerk, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
+  const { isSignedIn, isLoaded } = useUser();
+  const clerk = useClerk();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return; // Clerkのライブラリが読み込み中の場合は、何もしない
+    if (isSignedIn) router.push('/'); // ユーザーが既にサインイン済みの場合、トップページにリダイレクト
+  }, [isLoaded, isSignedIn, clerk, router]);
+
   return (
-    <RedirectToSignIn />
+    <>
+        <SignedIn>
+            <div>リダイレクトしています...</div>
+        </SignedIn>
+        <SignedOut>
+            <RedirectToSignIn />
+        </SignedOut>
+    </>
   );
 }
