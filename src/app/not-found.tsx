3155a35a59930050844791f7153af3671d300128
Ammonitedx <ammonitedx@gmail.com>
2025-06-19 22:45:10 +0900

    認証時の挙動を変更

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,14 +5,7 @@ export default function NotFound() {
   return (
     <main className="container mx-auto p-4 pt-8 max-w-2xl text-center">
       <h1 className="text-3xl font-bold text-yellow-600 mb-4">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-700">
-        Sorry, the page you are looking for does not exist.
-      </p>
-      <div className="mt-8">
-        <SignedOut>
-          <SignInButton mode='modal' />
-        </SignedOut>
-      </div>
+      <p className="text-lg text-gray-700">Sorry, the page you are looking for does not exist.</p>
     </main>
   );
 }
